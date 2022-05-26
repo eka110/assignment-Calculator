b13826176af0d43d2cb42ae9f435881e8217bb61
eka110 <plasticwatcher@gmail.com>
2022-05-26 18:46:16 +0400

    assignment calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+let display = Document.getElementById('display');
+
+let button = Array. from(document.getElementByClassName('button'));
+
+buttons.map( button => {
+button.addEventListener('click', (e) => {
+    switch(e.target.innerText){
+        case 'C':
+            display. innerText = '';
+            break; 
+            case '&larr':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+
+                }
+              
+break;
+case '=':
+    try{
+        display. innerText = eval(display.innerText);
+
+    }catch {
+        display. innerText = 'Error!';
+    }
+    
+break;
+        default:
+            display.innerText += e.target. innerText;
+    }
+ });
+});
